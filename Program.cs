97fb764c821cd9d7a71494de873effa21b06cd00
Someidiotter <anullowo@gmail.com>
2024-04-18 12:42:10 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Inheritance_Null_Asher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Inheritance_Null_Asher
+ 
