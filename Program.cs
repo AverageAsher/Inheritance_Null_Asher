ef6dae6a23ac2833fe4ca64c6c39044ac2b87f6a
Someidiotter <anullowo@gmail.com>
2024-04-18 12:46:29 -0500

    DONE 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,58 @@
 ﻿namespace Inheritance_Null_Asher
 {
-    internal class Program
+    using System;
+
+    // Abstract class Game
+    abstract class Game
+    {
+        // Automatic properties
+        public string ESRB { get; }
+        public string Title { get; }
+
+        // Constructor
+        public Game(string esrb, string title)
+        {
+            ESRB = esrb;
+            Title = title;
+        }
+
+        // Method to play the game
+        public void PlayGame()
+        {
+            Console.WriteLine($"{Title} is Starting");
+        }
+
+        // Abstract method to describe the game
+        public abstract string Describe();
+    }
+
+    // Inheriting class from Game
+    class FPS : Game
+    {
+        // Constructor inheriting from base class
+        public FPS(string esrb, string title) : base(esrb, title)
+        {
+        }
+
+        // Overriding Describe method
+        public override string Describe()
+        {
+            return $"This is a first-person shooter game titled {Title} with ESRB rating {ESRB}.";
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creating FPS object
+            FPS fpsGame = new FPS("Mature", "Call of Duty");
+
+            // Calling Describe method
+            Console.WriteLine(fpsGame.Describe());
+
+            // Calling PlayGame method
+            fpsGame.PlayGame();
         }
     }
 }
